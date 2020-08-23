629cea2c71c1ab87cefa5a755bb50b877a725574
unknown <13144744@student.uts.edu.au>
2020-08-24 01:37:00 +1000

    70 percent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
+    Vector3 pos1;
+    Vector3 pos2;
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject instance = (GameObject)Instantiate(redObj, transform.position, transform.rotation);
+        pos1 = new Vector3(2f, 0f, 0f);
+        pos2 = new Vector3(-2f, 0f, 0f);
+        _ = (GameObject)Instantiate(redObj, pos1, transform.rotation);
+        _ = (GameObject)Instantiate(blueObj, pos2, transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
