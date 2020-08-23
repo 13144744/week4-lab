a1f9f5fd01d4118b403883eb0498429602188fc5
unknown <13144744@student.uts.edu.au>
2020-08-24 00:50:18 +1000

    60 precent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
+    public GameObject redObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject instance = (GameObject)Instantiate(redObj, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Test");
+        
     }
 }
